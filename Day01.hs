e25c1587b0cb85065485d3159ffefc524300dfef
Jamie Curtis <jamocurtis@gmail.com>
2024-12-01 13:21:44 -0400

    day 1 part 2

--- Day01.hs
+++ Day01.hs
@@ -1,6 +1,12 @@
 import Data.List (sort)
 
+testInput = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3\n"
+
+-- part 1
+
+parse :: String -> [(Int, Int)]
 parse input = map parseLine (lines input)
+
 parseLine line = (read a, read b)
  where
   [a, b] = words line
@@ -11,8 +17,21 @@ solve input = sum (zipWith (curry absolute) (sort a) (sort b))
  where
   (a, b) = unzip (parse input)
 
-testInput = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3"
+test = solve testInput == 11
 
+-- echo "input" | runhaskell Day01
+-- powershell.exe -c Get-Clipboard | runhaskell Day01
 main = do
   input <- getContents
   print (solve input)
+  print (solve' input)
+
+-- part 2
+
+solve' input = sum $ map (count b) a
+ where
+  (a, b) = unzip (parse input)
+
+count list x = x * length (filter (== x) list)
+
+test' = solve' testInput == 31
